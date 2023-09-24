2d1a22f3e6bfbe45dbce0a352a997091e548a5f1
YuyaYamazi <kaimomo12345@gmail.com>
2023-09-24 16:25:21 +0900

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header className='text-gray-700 text-xl border-b-2 border-indigo-500'>
+        <div>
+          <a href='#'>
+            <span>ShinCode</span>
+          </a>
+          <nav>
+            <a href='#home'>Home</a>
+            <a href='#About'>About</a>
+            <a href='#Skills'>Skills</a>
+            <a href='#Grallery'>Grallery</a>
+          </nav>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
