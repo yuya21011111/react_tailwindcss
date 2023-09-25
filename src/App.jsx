99a16532000f6266bfd5d5f04023f15c58e613a3
YuyaYamazi <kaimomo12345@gmail.com>
2023-09-25 17:37:39 +0900

    レスポンシブ対応

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import './App.css'
 function App() {
   return (
     <>
-      <header className='text-gray-700 text-xl border-b-2 border-indigo-500'>
-        <div>
-          <a href='#'>
-            <span>ShinCode</span>
+      <header className='text-gray-500 text-xl border-b-2 border-indigo-500'>
+        <div className='container flex mx-auto p-5 flex-col md:flex-row items-center'>
+          <a className='font-medium text-gray-900' href='#'>
+            <span className='text-2xl ml-3'>ShinCode</span>
           </a>
-          <nav>
+          <nav className='md:ml-auto text-base'>
             <a href='#home'>Home</a>
             <a href='#About'>About</a>
             <a href='#Skills'>Skills</a>
